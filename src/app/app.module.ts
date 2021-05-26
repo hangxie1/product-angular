f5a5fce9e4f61e5abe4b11a06a4650c6e8d976d9
Hang Xie <pair+hang.xie@CVSHealth.com>
2021-05-26 07:57:17 -0500

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { OrderFormComponent } from './order-form/order-form.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
     OrderFormComponent
 
   ],
